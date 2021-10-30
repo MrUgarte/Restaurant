d8e57c0a4c3a96914718031c36ab794fa955608a
Marco Ugarte <marcougarte1998@gmail.com>
2021-10-30 15:03:08 -0300

    Carpeta Templates y cambio a URL y Settings

--- WebRestaurant/WebRestaurant/settings.py
+++ WebRestaurant/WebRestaurant/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1lu0ley=)0pl&-m4p9wth0bu%_kre1lt%fi2$!w!5to0m6uu+m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'WebRestaurant',
 ]
 
 MIDDLEWARE = [
