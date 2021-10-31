26d49dcd7f75e0551e20e8fdda58b5a64b5cfd97
Fel-gaetea <fel.gaetea@alumnos.duoc.cl>
2021-10-31 20:19:22 -0300

    cambios models

--- WebRestaurant/cliente/models.py
+++ WebRestaurant/cliente/models.py
@@ -1,5 +1,22 @@
 from django.db import models
+import uuid #Se requiere para los pruductos unicos
 
-# Create your models here.
+# Creacion de modelos.
 class Usuario(models.Model):
-    user = models.CharField(max_length=50)
+    user = models.CharField(max_length=100)
+    password=models.CharField(max_length=100)
+    Email=models.EmailField(max_length=200)
+
+
+class Sesion(models.model):
+    user = models.CharField(max_length=100)
+    password=models.CharField(max_length=100)
+
+    def _str_(self):
+        return self.user
+
+class Productos(models.model):
+    ID_Producto=models.UUIDField(primary_key=True,default=uuid.uuid4,help_text='id unica para producto')
+    Nombre_producto=models.CharField(max_length=100)
+    Stock=models.IntegerField(blank=True,Null=True)
+
