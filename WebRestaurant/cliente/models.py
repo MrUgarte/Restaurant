45c8090bd4d0835b287bc52c002829b3d520dc30
Marco Ugarte <marcougarte1998@gmail.com>
2021-10-31 19:39:02 -0300

    ACt 2.0

--- WebRestaurant/cliente/models.py
+++ WebRestaurant/cliente/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    user = models.CharField(max_length=50)
--- WebRestaurant/cliente/views.py
+++ WebRestaurant/cliente/views.py
@@ -1,12 +1,28 @@
 from typing import ContextManager
-from django.shortcuts import render
+from .forms import CustomUserCreationForm
+from django.shortcuts import redirect, render
 from django.views.generic import View
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+
 
 # Create your views here.
 class ClienteListViews(View):
     def get(self, request, *args, **kwargs):
         context={
-
+            
         }   
-        return render(request, 'registro.html', context)
+        
+        # FORMULARIO DE REGISTRO
+        data ={  
+            'form':CustomUserCreationForm() 
+        }   
+        
+        if request.method == 'POST':
+            formulario = CustomUserCreationForm(data=request.POST)
+            if formulario.is_valid():
+                formulario.save()
+                user = authenticate(username=formulario.cleaned_data["username"],password=formulario.cleaned_data["password1"])
+                return redirect(to="login")
+        
+        return render(request, 'registro.html', data)
