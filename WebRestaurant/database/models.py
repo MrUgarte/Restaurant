e6fb9452e192cadfd605a685d46eaacd3ef4c228
Marco Ugarte <marcougarte1998@gmail.com>
2021-11-04 03:53:01 -0300

    Act

--- WebRestaurant/database/models.py
+++ WebRestaurant/database/models.py
@@ -6,6 +6,7 @@ class plato(models.Model):
     nombre=models.CharField(max_length=50)
     precio=models.IntegerField()
     descripcion=models.TextField()
+    imagen = models.ImageField(upload_to="media/platos", null=True)
 
     def __str__(self) :
         return self.nombre
